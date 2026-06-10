4bdae298aeb6e5bdf1b213f50d432b2fccea1c74
faaizvahora-lab <faaizvahora@gmail.com>
2026-06-10 15:02:34 +0530

    new update

--- day8/count-vowels.py
+++ day8/count-vowels.py
@@ -1,5 +1,6 @@
 word = input("Enter a word: ")
 vowel = "aeiouAEIOU"
+Non_vowel = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWYZ"
 vowel_count = 0
 Non_vowel_count = 0
 count=0
@@ -8,7 +9,12 @@ for char in word:
     if char.isalpha():
         if char in vowel:
            vowel_count += 1
-else:
-    Non_vowel_count += 1
+
+for let in word:
+     if let.isalpha():
+        if let in Non_vowel:
+            Non_vowel_count += 1
+
+    
 print(f"Vowels = {vowel_count}")
-print(f"non-vowels:", Non_vowel_count)
+print(f"Non-vowels = {Non_vowel_count}")
